24f021afd301bb69da00de8e7b1d1f138319577e
Lysin Kirill <lysinkirill@gmail.com>
2024-03-28 03:22:02 +0300

    fix: player no longer sticks to walls

--- Assets/_Source/Core/PlayerMovement.cs
+++ Assets/_Source/Core/PlayerMovement.cs
@@ -1,25 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 
 namespace Core
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField]
-        private float jumpStrength;
-        
-        [SerializeField]
-        private float walkSpeed;
+        [SerializeField] private float jumpStrength;
+        [SerializeField] private float walkSpeed;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private LayerMask wallsLayer;
+        [SerializeField] private BoxCollider2D groundCheckCollider;
+        [SerializeField] private BoxCollider2D wallCheckCollider;
 
-        public LayerMask groundLayer;
+        private float _horizontal;
+        private bool _isFacingRight = true;
 
-        public BoxCollider2D groundCheck;
-
-        private float horizontal;
-        private bool isFacingRight = true;
-
-        public bool isGrounded;
+        private bool _isGrounded;
+        public bool _isTouchingWall;
         
         private Rigidbody2D _body;
         private PlayerInputActions _playerInputActions;
@@ -36,25 +35,26 @@ namespace Core
         private void FixedUpdate()
         {
             CheckGround();
+            CheckWalls();
             var velocity = _body.velocity;
             
-            if (isGrounded)
+            if (_isGrounded)
             {
                 velocity.x *= DragCoefficient;
                 _body.velocity = velocity;
             }
 
-            if (horizontal != 0)
-                _body.velocity = new Vector2(horizontal * walkSpeed, _body.velocity.y);
+            if (_horizontal != 0 && (!_isTouchingWall || _isGrounded))
+                _body.velocity = new Vector2(_horizontal * walkSpeed, _body.velocity.y);
             
-            if (!isFacingRight && horizontal > 0 || isFacingRight && horizontal < 0)
+            if (!_isFacingRight && _horizontal > 0 || _isFacingRight && _horizontal < 0)
                 Flip();
         }
         
 
         public void Jump(InputAction.CallbackContext context)
         {
-            if (context.performed && isGrounded)
+            if (context.performed && _isGrounded)
                 _body.velocity = new Vector2(_body.velocity.x, jumpStrength);
 
             var velocity = _body.velocity;
@@ -64,19 +64,25 @@ namespace Core
 
         public void Move(InputAction.CallbackContext context)
         {
-            horizontal = context.ReadValue<Vector2>().x;
+            _horizontal = context.ReadValue<Vector2>().x;
         }
 
 
         void CheckGround()
         {
-            var bounds = groundCheck.bounds;
-            isGrounded = Physics2D.OverlapAreaAll(bounds.min, bounds.max, groundLayer).Length > 0;
+            var bounds = groundCheckCollider.bounds;
+            _isGrounded = Physics2D.OverlapAreaAll(bounds.min, bounds.max, groundLayer).Length > 0;
+        }
+
+        void CheckWalls()
+        {
+            var bounds = wallCheckCollider.bounds;
+            _isTouchingWall = Physics2D.OverlapAreaAll(bounds.min, bounds.max, wallsLayer).Length > 0;
         }
 
         private void Flip()
         {
-            isFacingRight = !isFacingRight;
+            _isFacingRight = !_isFacingRight;
             var localScale = transform.localScale;
             localScale.x *= -1;
             transform.localScale = localScale;
