64c91b45c0a4513f1f2658b4225caacab2b8064e
Lysin Kirill <lysinkirill@gmail.com>
2024-03-28 06:50:51 +0300

    feat: add stacking system; add item pick up system

---
+++ Assets/_Source/Core/PickUpSystem/PickUpSystem.cs
@@ -0,0 +1,24 @@
+﻿using ScriptableObjects;
+using System;
+using UnityEngine;
+
+namespace Core.PickUpSystem
+{
+    public class PickUpSystem : MonoBehaviour
+    {
+        [SerializeField] private InventoryAsset inventoryData;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            PickableItem item = collision.GetComponent<PickableItem>();
+            if (item == null)
+                return;
+
+            int remainder = inventoryData.AddItem(item.Item, item.Quantity);
+            if (remainder == 0)
+                item.DestroyItem();
+            else
+                item.Quantity = remainder;
+        }
+    }
+}
--- Assets/_Source/ScriptableObjects/InventoryAsset.cs
+++ Assets/_Source/ScriptableObjects/InventoryAsset.cs
@@ -1,6 +1,7 @@
 using Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ScriptableObjects
@@ -8,30 +9,96 @@ namespace ScriptableObjects
     [CreateAssetMenu(fileName = "Item", menuName = "SO/newInventory")]
     public class InventoryAsset : ScriptableObject
     {
-        [SerializeField] private List<InventoryItemModel> inventoryItems;
+        [SerializeField]
+        private List<InventoryItemModel> inventoryItems;
 
         [field: SerializeField] public int Size { get; private set; } = 10;
 
-        public event Action<Dictionary<int, InventoryItemModel>> OnInventoryUpdated; 
+        public event Action<Dictionary<int, InventoryItemModel>> OnInventoryUpdated;
+
         public void Init()
         {
             inventoryItems = new List<InventoryItemModel>();
-            for(int i = 0; i < Size; ++i)
+            for (int i = 0; i < Size; ++i)
                 inventoryItems.Add(InventoryItemModel.Empty);
         }
 
-        public void AddItem(ItemAsset itemAsset, int quantity)
+        public int AddItem(ItemAsset itemAsset, int quantity)
         {
+            if (!itemAsset.IsStackable)
+            {
+                for (int i = 0; i < inventoryItems.Count; ++i)
+                {
+                    while (quantity > 0 && !IsInventoryFull())
+                    {
+                        quantity -= AddItemToFirstEmptySlot(itemAsset, 1);
+                    }
+
+                    NotifyAboutUpdate();
+                    return quantity;
+                }
+
+            }
+
+            quantity = AddStackableItem(itemAsset, quantity);
+            NotifyAboutUpdate();
+            return quantity;
+        }
+
+        private int AddItemToFirstEmptySlot(ItemAsset itemAsset, int quantity)
+        {
+            InventoryItemModel newItem = new InventoryItemModel
+            {
+                quantity = quantity,
+                item = itemAsset,
+            };
+
             for (int i = 0; i < inventoryItems.Count; ++i)
+            {
                 if (inventoryItems[i].IsEmpty)
                 {
-                    inventoryItems[i] = new InventoryItemModel
-                    {
-                        quantity = quantity,
-                        item = itemAsset,
-                    };
-                    return;
+                    inventoryItems[i] = newItem;
+                    return quantity;
                 }
+            }
+
+            return 0;
+        }
+
+        private bool IsInventoryFull() => !inventoryItems.Any(item => item.IsEmpty);
+
+        private int AddStackableItem(ItemAsset itemAsset, int quantity)
+        {
+            for (int i = 0; i < inventoryItems.Count; ++i)
+            {
+                if (inventoryItems[i].IsEmpty)
+                    continue;
+                if (inventoryItems[i].item.ID == itemAsset.ID)
+                {
+                    int availableSpace = itemAsset.StackSize - inventoryItems[i].quantity;
+
+                    if (quantity > availableSpace)
+                    {
+                        inventoryItems[i] = inventoryItems[i] with { quantity = itemAsset.StackSize };
+                        quantity -= availableSpace;
+                    }
+                    else
+                    {
+                        inventoryItems[i] = inventoryItems[i] with { quantity = quantity + inventoryItems[i].quantity };
+                        NotifyAboutUpdate();
+                        return 0;
+                    }
+                }
+            }
+
+            while (quantity > 0 && !IsInventoryFull())
+            {
+                int newQuantity = Mathf.Clamp(quantity, 0, itemAsset.StackSize);
+                quantity -= newQuantity;
+                AddItemToFirstEmptySlot(itemAsset, newQuantity);
+            }
+
+            return quantity;
         }
 
         public void AddItem(InventoryItemModel item) => AddItem(item.item, item.quantity);
@@ -57,7 +124,7 @@ namespace ScriptableObjects
 
         public void SwapItems(int firstItemIndex, int secondItemIndex)
         {
-            if(firstItemIndex == - 1 || secondItemIndex == - 1)
+            if (firstItemIndex == -1 || secondItemIndex == -1)
                 return;
             (inventoryItems[firstItemIndex], inventoryItems[secondItemIndex]) = (inventoryItems[secondItemIndex], inventoryItems[firstItemIndex]);
             NotifyAboutUpdate();
