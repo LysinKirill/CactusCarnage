9fa3c3246886082051bd264e1572d7839cd3e6eb
Lysin Kirill <lysinkirill@gmail.com>
2024-04-14 20:18:34 +0300

    feat: changed drop mechanic

---
+++ Assets/_Source/Core/Enemies/DropHandler.cs
@@ -0,0 +1,48 @@
+﻿using ScriptableObjects.Enemies;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Core.Enemies
+{
+    public class DropHandler : MonoBehaviour
+    {
+        [SerializeField] private GameObject pickups;
+        [SerializeField] private LootTableAsset lootTable;
+
+        private readonly Random _random = new Random();
+
+        private void Awake()
+        {
+            if (!TryGetComponent(out EnemyHealth health))
+                return;
+
+            health.OnDeath += AttemptToGenerateLoot;
+        }
+
+        private void AttemptToGenerateLoot(GameObject enemy)
+        {
+            //if (lootTable == null)
+            //    return;
+            List<GameObject> prefabsToInstantiate = new List<GameObject>();
+            foreach (var lootDescription in lootTable.lootDescriptions)
+            {
+                var randValue = _random.Next(0, 101);
+                if (randValue <= lootDescription.dropChancePercentage)
+                    prefabsToInstantiate.Add(lootDescription.lootPrefab);
+            }
+
+            foreach (GameObject prefab in prefabsToInstantiate)
+            {
+                var droppedItem = Instantiate(
+                    prefab,
+                    enemy.transform.position,
+                    Quaternion.identity
+                );
+                if (pickups != null)
+                    droppedItem.transform.SetParent(pickups.transform);
+            }
+        }
+    }
+}
---
+++ Assets/_Source/ScriptableObjects/Enemies/LootTableAsset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ScriptableObjects.Enemies
+{
+    [CreateAssetMenu(fileName = "LootTable", menuName = "SO/Enemies/newLootTableAsset")]
+    public class LootTableAsset : ScriptableObject
+    {
+        [field: SerializeField] public List<LootDescription> lootDescriptions;
+    }
+
+    [Serializable]
+    public class LootDescription
+    {
+        public GameObject lootPrefab;
+        [Range(0, 100)] public int dropChancePercentage;
+    };
+}
